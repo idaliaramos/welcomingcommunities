7d3a6579c8489c0a1fb9f0ffb5e1c30d2913275b
NikSS <nik.safe@gmail.com>
2018-11-03 15:59:49 -0700

    create the map component for landing page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import Card from './components/CardComponent'
+// import Card from './components/CardComponent'
 import './App.css';
 import CulturePage from './components/CulturePage'
+import Map from './components/Map'
+console.log(Map,"<<<map")
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-
+        <Map />
           <CulturePage/>
         </header>
       </div>
