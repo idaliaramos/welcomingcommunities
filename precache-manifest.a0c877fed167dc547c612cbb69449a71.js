d5532732b131f291826030c08d8f71cb34748da2
swain <iamswain@hotmail.com>
2018-11-04 11:57:51 -0800

    Updates

---
+++ precache-manifest.a0c877fed167dc547c612cbb69449a71.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/welcomingcommunities/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "ddba53c133a11ef6f969",
+    "url": "/welcomingcommunities/static/css/main.7221ea49.chunk.css"
+  },
+  {
+    "revision": "f7762c3faaa5c741c7edc37c6c6572b7",
+    "url": "/welcomingcommunities/static/media/Voluntary-Agencies-geocode.f7762c3f.csv"
+  },
+  {
+    "revision": "3466c70b1a5ab8e3e26b",
+    "url": "/welcomingcommunities/static/js/1.3466c70b.chunk.js"
+  },
+  {
+    "revision": "5b254bfa5710e13bdeb4",
+    "url": "/welcomingcommunities/static/js/runtime~main.5b254bfa.js"
+  },
+  {
+    "revision": "d5fa5613daddab432d9c3834987062a1",
+    "url": "/welcomingcommunities/static/media/culturexplore.d5fa5613.png"
+  },
+  {
+    "revision": "88733d26bdbbac1b798bbcd57c3caa1c",
+    "url": "/welcomingcommunities/static/media/us-state-names.88733d26.tsv"
+  },
+  {
+    "revision": "09979f01bd8f5948394bda95dddb806d",
+    "url": "/welcomingcommunities/static/media/cities.09979f01.csv"
+  },
+  {
+    "revision": "06c24dfb0de0ead358e5f0a1fd948c7f",
+    "url": "/welcomingcommunities/static/media/grouped_by_and_sum.06c24dfb.csv"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/welcomingcommunities/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/welcomingcommunities/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/welcomingcommunities/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/welcomingcommunities/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ddba53c133a11ef6f969",
+    "url": "/welcomingcommunities/static/js/main.ddba53c1.chunk.js"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/welcomingcommunities/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/welcomingcommunities/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/welcomingcommunities/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/welcomingcommunities/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/welcomingcommunities/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/welcomingcommunities/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/welcomingcommunities/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/welcomingcommunities/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/welcomingcommunities/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/welcomingcommunities/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/welcomingcommunities/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "3466c70b1a5ab8e3e26b",
+    "url": "/welcomingcommunities/static/css/1.7d0059e8.chunk.css"
+  },
+  {
+    "revision": "8485cdd15465828c8a408526f154028b",
+    "url": "/welcomingcommunities/index.html"
+  }
+];
