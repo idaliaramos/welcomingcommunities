4b469e1a2e49b64b05f2b38426a62b3abc15f02b
swain <iamswain@hotmail.com>
2018-11-04 10:18:50 -0800

    added customed map

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import Card from './components/CardComponent'
-import './App.css';
-import LandingPage from './components/LandingPage'
-import CulturePage from './components/CulturePage'
-import Map from './components/Map'
+import "./App.css";
+import LandingPage from "./components/LandingPage";
+import CulturePage from "./components/CulturePage";
+import Map from "./components/Map";
 var api_key = process.env.API_KEY;
 
-
 class App extends Component {
-  state={
-    data:[],
-  }
+  state = {
+    data: []
+  };
   data = async () => {
-    let data = await fetch('https:api.airtable.com/v0/appQVHyUAtu8PtEYC/CountryInfo?maxRecords=3&view=Grid%20view',{
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer keyvXxrgLJDpKDquk`
-    }
-  }
-)
-  let res = await data.json();
-  let countryInfo = res.records.map(e => e.fields)
-  this.setState({data: countryInfo})
-  }
+    let data = await fetch(
+      "https:api.airtable.com/v0/appQVHyUAtu8PtEYC/CountryInfo?maxRecords=3&view=Grid%20view",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer keyvXxrgLJDpKDquk`
+        }
+      }
+    );
+    let res = await data.json();
+    let countryInfo = res.records.map(e => e.fields);
+    this.setState({ data: countryInfo });
+  };
   componentDidMount() {
-    this.data()
-    }
+    this.data();
+  }
   render() {
-
     return (
       <div className="App">
         <header className="App-header">
-
-         <LandingPage/>
+          <LandingPage />
         </header>
-
-          <CulturePage countries={this.state.data}/>
-
+        <Map></Map>
+        <CulturePage countries={this.state.data} />
       </div>
     );
   }
